4b8fd82c24c4a16d13e6f35e4190dd2f6f05183c
Michael <Michael@gmail.com>
2024-10-31 22:13:38 +0300

    sum function

---
+++ 1901075_MichaelJoseph_main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int my_sum(int x,int y);
+
+int main()
+{
+        cout<<"hello world";
+
+        return 0;
+}
+
+
+
+int my_sum (int x , int y)
+
+{
+    return x+y;
+}
